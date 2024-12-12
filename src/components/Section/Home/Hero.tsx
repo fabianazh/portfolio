c7b202204077daa17df5d7dc78d8a255f6b32573
fabianazh <fabianazhar216@gmail.com>
2024-12-13 06:35:04 +0700

    Updating sitemap

--- src/components/Section/Home/Hero.tsx
+++ src/components/Section/Home/Hero.tsx
@@ -28,10 +28,10 @@ export default function Hero() {
             {/* Hero Section */}
             <section
                 id="home"
-                className="z-0 w-screen h-fit lg:h-screen py-4 lg:py-6 px-0 lg:px-12 z-0"
+                className="z-0 w-screen h-fit lg:h-screen py-4 lg:py-6 px-0 lg:px-12"
             >
                 {/* Content Container */}
-                <div className="relative w-full h-full flex flex-col justify-between bg-stone-200/30 shadow-sm rounded-2xl px-4 lg:px-10 py-4 lg:py-6">
+                <div className="relative w-full h-full flex flex-col justify-between bg-stone-200/30 shadow-sm rounded-2xl px-0 lg:px-10 py-4 lg:py-6">
                     {/* Background */}
                     <BackgroundBeams className="absolute w-full h-full top-0 left-0" />
                     {/* End Background */}
@@ -104,7 +104,7 @@ export default function Hero() {
                                 href={
                                     'mailto:fabianazharr@gmail.com?subject=Project%20Request&body=Hello%20Muhammad%20Fabian%20Azhar,%0D%0A%0D%0AI%20would%20like%20to%20discuss%20a%20potential%20project%20with%20you.%20Please%20let%20me%20know%20a%20convenient%20time%20for%20us%20to%20talk%20further.%0D%0A%0D%0AThank%20you!%0D%0A%0D%0ABest%20regards,%0D%0A%5BYour%20Name%5D'
                                 }
-                                className="w-fit text-xs lg:text-sm font-medium py-2 px-4 lg:px-6 mt-4 lg:mt-6 inline-flex h-fit gap-2 bg-white rounded-full items-center shadow-sm z-10 hover:bg-stone-50 transition-colors duration-300 z-10"
+                                className="w-fit text-xs lg:text-sm font-medium py-2 px-4 lg:px-6 mt-4 lg:mt-6 inline-flex h-fit gap-2 bg-white rounded-full items-center shadow-sm z-10 hover:bg-stone-50 transition-colors duration-300"
                             >
                                 <MailIcon className="w-fit h-4 lg:h-5" />{' '}
                                 Request a project
